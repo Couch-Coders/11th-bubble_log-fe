711ab3d013aa5bec3dbfb73d2d5c71b0521f47a1
nonon <nononcrust@gmail.com>
2022-10-07 23:34:45 +0900

    chore: Remove unused codes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,7 @@
 import React from 'react'
-import { useDispatch, useSelector } from '@stores/index'
-import {
-  fetchHotCoffees,
-  fetchHotCoffeesWithError
-} from '@stores/slices/coffee'
 
 const Home: React.FC = () => {
-  const dispatch = useDispatch()
-  const { data, isLoading, error } = useSelector((state) => state.coffee)
-
-  const onClickFetchButton = async (): Promise<void> => {
-    await dispatch(fetchHotCoffees())
-  }
-
-  const onClickFetchButtonWithError = async (): Promise<void> => {
-    await dispatch(fetchHotCoffeesWithError())
-  }
-
-  return (
-    <>
-      {isLoading && <p>loading...</p>}
-      {error != null && <p>error</p>}
-      {data.map((coffee) => (
-        <p key={coffee.id}>{coffee.title}</p>
-      ))}
-      <button
-        onClick={() => {
-          void onClickFetchButton()
-        }}
-      >
-        Fetch
-      </button>
-      <button
-        onClick={() => {
-          void onClickFetchButtonWithError()
-        }}
-      >
-        Fetch With Error
-      </button>
-    </>
-  )
+  return <div>Home</div>
 }
 
 export default Home
