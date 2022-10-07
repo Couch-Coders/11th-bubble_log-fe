c58788b267d6d29055e3ed8f1718616ae2b30d23
minju20200305 <minju.kwon.0417@gmail.com>
2022-10-07 15:26:15 +0900

    feat: Logo add

--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -3,11 +3,6 @@ import reset from 'styled-reset'
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-  body{
-  width: 420px;
-  background-color: gray;
-  background-position: center center;
-}
 `
 
 export default GlobalStyle
