f7f242fc279ac76dd81facf2856ab089a4ed95d9
nonon <nononcrust@gmail.com>
2022-10-11 14:27:41 +0900

    WriteLogButton 컴포넌트 추가

---
+++ src/components/WriteLogButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const WriteLogButton: React.FC = () => {
+  return (
+    <Link to='/write'>로그 작성</Link>
+  )
+}
+
+export default WriteLogButton
--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import LogList from '@components/LogList'
 import SearchInput from '@components/SearchInput'
+import WriteLogButton from '@components/WriteLogButton'
 
 const Logs: React.FC = () => {
   return <main>
     <SearchInput />
     <LogList />
+    <WriteLogButton />
   </main>
 }
 
