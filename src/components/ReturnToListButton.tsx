c0254425e08c1fa3fe206fab44c125b4242dac71
nonon <nononcrust@gmail.com>
2022-10-11 16:36:02 +0900

    feat: ReturnToListButton 컴포넌트 추가

---
+++ src/components/ReturnToListButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ReturnToListButton: React.FC = () => {
+  return (
+    <Link to='/logs'>
+      <button>돌아가기</button>
+    </Link>
+  )
+}
+
+export default ReturnToListButton
