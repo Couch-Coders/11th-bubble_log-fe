f696a86d92a09992e8df6b91b6a97ee81e9112e0
nonon <nononcrust@gmail.com>
2022-10-14 17:23:02 +0900

    feat: Dropdown 컴포넌트 추가

---
+++ src/components/common/dropdown/DropdownButton.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { MdArrowDropDown } from 'react-icons/md';
+import styled, { css } from 'styled-components';
+
+import { gray } from '@styles/palette';
+
+type DropdownButtonSizeType = 'small' | 'medium' | 'large';
+
+const getDropdownButtonSize = (size?: DropdownButtonSizeType): any => {
+  switch (size) {
+    case 'small':
+      return css`
+        height: 2rem;
+        padding: 0.75rem;
+        font-size: 0.75rem;
+        min-width: 4rem;
+      `;
+    case 'medium':
+      return css`
+        height: 2.5rem;
+        padding: 0.75rem;
+        font-size: 0.875rem;
+        min-width: 5rem;
+      `;
+    case 'large':
+      return css`
+        height: 3rem;
+        padding: 0.75rem;
+        font-size: 1rem;
+        min-width: 6rem;
+      `;
+  }
+};
+
+interface ContainerProps {
+  size?: DropdownButtonSizeType;
+  open: boolean;
+}
+
+const Container = styled.button<ContainerProps>`
+  position: relative;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.5rem;
+
+  font-weight: 500;
+  border-radius: 0.5rem;
+  border: 1px solid ${gray[300]};
+  background-color: white;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${gray[100]};
+  }
+
+  ${({ size }) => getDropdownButtonSize(size)};
+
+  ${({ open }) =>
+    open &&
+    css`
+      .arrow-dropdown-icon {
+        transform: rotate(180deg);
+      }
+    `}
+`;
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  label?: string;
+  size?: DropdownButtonSizeType;
+}
+
+const DropdownButton: React.FC<Props> = ({
+  children,
+  label = '메뉴',
+  size = 'medium',
+  ...props
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const onClick = (): void => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <Container size={size} open={open} onClick={onClick} {...props}>
+      {label}
+      <MdArrowDropDown
+        className="arrow-dropdown-icon"
+        size="1.25rem"
+        color={gray[500]}
+      />
+      {open && children}
+    </Container>
+  );
+};
+
+export default DropdownButton;
---
+++ src/components/common/dropdown/index.ts
@@ -0,0 +1,11 @@
+import DropdownButton from '@components/common/dropdown/DropdownButton';
+import DropdownMenu from '@components/common/dropdown/DropdownMenu';
+import DropdownMenuItem from '@components/common/dropdown/DropdownMenuItem';
+
+const Dropdown = {
+  Button: DropdownButton,
+  Menu: DropdownMenu,
+  MenuItem: DropdownMenuItem,
+};
+
+export default Dropdown;
