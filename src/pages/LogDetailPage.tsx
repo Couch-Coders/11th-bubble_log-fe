a817849b0e9acc65f1e08912265fc5558034b496
nonon <nononcrust@gmail.com>
2022-10-14 21:16:22 +0900

    fix: 조회 컴포넌트가 에러를 렌더링하지 않도록 수정

--- src/pages/LogDetailPage.tsx
+++ src/pages/LogDetailPage.tsx
@@ -9,7 +9,7 @@ import ToggleFavoriteButton from '@components/ToggleFavoriteButton';
 const LogDetailPage: React.FC = () => {
   const dispatch = useDispatch();
 
-  const { data, isLoading, error } = useSelector((state) => state.logDetail);
+  const { data, isLoading } = useSelector((state) => state.logDetail);
 
   const params = useParams();
 
@@ -24,8 +24,7 @@ const LogDetailPage: React.FC = () => {
   return (
     <main>
       {isLoading && <p>loading...</p>}
-      {error !== null && <p>error</p>}
-      {data !== null && <p>{data.content}</p>}
+      {data !== null && <p>{JSON.stringify(data)}</p>}
       {data !== null && (
         <ToggleFavoriteButton isFavorite={data.isFavorite} logId={logId} />
       )}
