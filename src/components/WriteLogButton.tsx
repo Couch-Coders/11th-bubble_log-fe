ace13c3a471d20de4a8368bf904f04373ec0883f
nonon <nononcrust@gmail.com>
2022-10-15 20:42:27 +0900

    fix: WriteLogButton 컴포넌트 스타일 수정

--- src/components/WriteLogButton.tsx
+++ src/components/WriteLogButton.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Button from './common/Button';
 
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
 const WriteLogButton: React.FC = () => {
   return (
-    <Link to="/write">
-      <Button>로그 작성</Button>
-    </Link>
+    <Container>
+      <Link to="/write">
+        <Button>로그 작성</Button>
+      </Link>
+    </Container>
   );
 };
 
