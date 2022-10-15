2cfdee1b2d4d8bcc037b6b74fec7709bfe1a7baf
nonon <nononcrust@gmail.com>
2022-10-15 23:17:24 +0900

    feat: UnregisterButton 컴포넌트 추가

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,27 +1,17 @@
 import { useSelector } from '@stores/index';
 import React from 'react';
 
-import { unregisterAPI } from '@apis/auth';
 import Layout from '@components/Layout';
+import UnregisterButton from '@components/UnregisterButton';
 
 const MyPage: React.FC = () => {
   const { data } = useSelector((state) => state.user);
-
-  const onClickUnregisterButton = async (): Promise<void> => {
-    await unregisterAPI();
-  };
 
   return (
     <Layout>
       <p>{data.name}</p>
       <p>{data.email}</p>
-      <button
-        onClick={() => {
-          void onClickUnregisterButton;
-        }}
-      >
-        탈퇴하기
-      </button>
+      <UnregisterButton />
     </Layout>
   );
 };
