096fa739143ec7798de99cc901b669dcb0205df9
nonon <nononcrust@gmail.com>
2022-10-18 23:18:38 +0900

    refactor: 파이어베이스 설정 파일 분리

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,15 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyBTDQZ4eVn-ej-n0XlmSakjvmmsBlNakBE',
-  authDomain: 'th-bubble-log.firebaseapp.com',
-  projectId: 'th-bubble-log',
-  storageBucket: 'th-bubble-log.appspot.com',
-  messagingSenderId: '629745699260',
-  appId: '1:629745699260:web:b5659deb042b4f410e43b9',
-  measurementId: 'G-1J3K5XGW8D',
-};
+import { config } from '@services/firebaseConfig';
 
 const app = initializeApp(config);
 const auth = getAuth(app);
