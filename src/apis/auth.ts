493f5ef3aaf3e1c2db14c2a7eda4d13a2af8dda1
nonon <nononcrust@gmail.com>
2022-10-18 21:39:51 +0900

    refactor: api 엔드포인트에 하드코딩 되어있는 값 수정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -2,19 +2,22 @@ import { LoginResponse } from 'types/auth';
 
 import { axios } from '@apis/index';
 
+const BASE_URL_AUTH = '/auth';
+const BASE_URL_USER = '/users';
+
 export const loginAPI = async (token: string): Promise<any> => {
   axios.defaults.headers.Authorization = `Bearer ${token}`;
 
-  const response = await axios.post<LoginResponse>('/auth/login');
+  const response = await axios.post<LoginResponse>(`${BASE_URL_AUTH}/login`);
   return response.data;
 };
 
 export const logoutAPI = async (): Promise<any> => {
-  const response = await axios.delete('/auth/logout');
+  const response = await axios.delete(`${BASE_URL_AUTH}/logout`);
   return response.data;
 };
 
 export const unregisterAPI = async (): Promise<any> => {
-  const response = await axios.delete('/users/me');
+  const response = await axios.delete(`${BASE_URL_USER}/me`);
   return response.data;
 };
--- src/apis/log.ts
+++ src/apis/log.ts
@@ -14,8 +14,13 @@ import {
 import { axios } from '@apis/index';
 import { createQueryString } from '@utils/createQueryString';
 
+const BASE_URL_LOGS = '/logs';
+
 export const createLogAPI = async (body: CreateLogBody): Promise<any> => {
-  const response = await axios.post<CreateLogResponse>('/logs', body);
+  const response = await axios.post<CreateLogResponse>(
+    `${BASE_URL_LOGS}`,
+    body,
+  );
   return response.data;
 };
 
@@ -23,29 +28,36 @@ export const updateLogAPI = async (
   body: UpdateLogBody,
   logId: number,
 ): Promise<any> => {
-  const response = await axios.put<UpdateLogResponse>(`/logs/${logId}`, body);
+  const response = await axios.put<UpdateLogResponse>(
+    `${BASE_URL_LOGS}/${logId}`,
+    body,
+  );
   return response.data;
 };
 
 export const deleteLogAPI = async (logId: number): Promise<any> => {
-  const response = await axios.delete(`/logs/${logId}`);
+  const response = await axios.delete(`${BASE_URL_LOGS}/${logId}`);
   return response.data;
 };
 
 export const getLogsAPI = async (query: GetLogsQuery): Promise<any> => {
   const queryString = createQueryString(query);
-  const response = await axios.get<GetLogsResponse>(`/logs${queryString}`);
+  const response = await axios.get<GetLogsResponse>(
+    `${BASE_URL_LOGS}${queryString}`,
+  );
   return response.data;
 };
 
 export const getLogDetailAPI = async (logId: number): Promise<any> => {
-  const response = await axios.get<GetLogDetailResponse>(`/logs/${logId}`);
+  const response = await axios.get<GetLogDetailResponse>(
+    `${BASE_URL_LOGS}/${logId}`,
+  );
   return response.data;
 };
 
 export const toggleLogFavoriteAPI = async (logId: number): Promise<any> => {
   const response = await axios.put<ToggleLogFavoriteRepsonse>(
-    `/logs/${logId}/favorite`,
+    `${BASE_URL_LOGS}/${logId}/favorite`,
   );
   return response.data;
 };
@@ -55,7 +67,7 @@ export const createLogImagesAPI = async (
   logId: number,
 ): Promise<any> => {
   const response = await axios.post<CreateLogImagesResponse>(
-    `/logs/${logId}/images`,
+    `${BASE_URL_LOGS}/${logId}/images`,
     body,
   );
   return response.data;
@@ -65,7 +77,9 @@ export const getLogImageAPI = async (
   logId: number,
   imageName: string,
 ): Promise<any> => {
-  const response = await axios.get(`/logs/${logId}/images/${imageName}`);
+  const response = await axios.get(
+    `${BASE_URL_LOGS}/${logId}/images/${imageName}`,
+  );
   return response.data;
 };
 
@@ -73,7 +87,9 @@ export const deleteLogImageAPI = async (
   logId: number,
   imageName: string,
 ): Promise<any> => {
-  const response = await axios.delete(`/logs/${logId}/images/${imageName}`);
+  const response = await axios.delete(
+    `${BASE_URL_LOGS}/${logId}/images/${imageName}`,
+  );
   return response.data;
 };
 
