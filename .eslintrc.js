001519aad20197b74ce353559db96c723bdc2493
nonon <nononcrust@gmail.com>
2022-10-19 13:00:41 +0900

    chore: 리액트 훅 관련 린트 룰 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,8 +25,10 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'import', 'preferred-import'],
+  plugins: ['react', 'import', 'preferred-import', 'react-hooks'],
   rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'import/order': [
       'error',
       {
