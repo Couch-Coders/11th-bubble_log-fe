10ae67c20917be02e1880f861c933738ef851f1d
nonon <nononcrust@gmail.com>
2022-10-21 03:27:19 +0900

    feat: TimePicker 컴포넌트 추가

---
+++ src/components/TimePicker.tsx
@@ -0,0 +1,79 @@
+import useOutsideClick from '@hooks/useOutsideClick';
+import { gray } from '@lib/styles/palette';
+import { format } from 'date-fns';
+import React, { useRef, useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
+import styled from 'styled-components';
+
+const TimePickerButton = styled.button`
+  border-radius: 0.25rem;
+  border: 1px solid ${gray[400]};
+  background-color: white;
+  color: ${gray[600]};
+  font-size: 1.25rem;
+  padding: 0.5rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${gray[100]};
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  width: 12rem;
+  flex-direction: column;
+  gap: 1rem;
+  position: relative;
+
+  .time-picker-container {
+    position: absolute;
+    top: 3.5rem;
+    z-index: 3;
+  }
+`;
+
+interface Props {
+  startTime: Date;
+  onChange: (
+    date: Date | null,
+    event?: React.SyntheticEvent<any, Event> | undefined,
+  ) => void;
+}
+
+const TimePicker: React.FC<Props> = ({ startTime, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleChange = (date: Date | null) => {
+    onChange(date);
+    setIsOpen(false);
+  };
+
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useOutsideClick(ref, () => setIsOpen(false));
+
+  return (
+    <Container ref={ref}>
+      <TimePickerButton onClick={() => setIsOpen((prev) => !prev)}>
+        {format(startTime, 'h:mm aa')}
+      </TimePickerButton>
+      {isOpen && (
+        <div className="time-picker-container">
+          <ReactDatePicker
+            selected={startTime}
+            onChange={handleChange}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            timeCaption="Time"
+            dateFormat="h:mm aa"
+            inline
+          />
+        </div>
+      )}
+    </Container>
+  );
+};
+
+export default TimePicker;
