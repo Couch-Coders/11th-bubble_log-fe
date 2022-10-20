eb66fffe4b947133f5123584a2fe89ce8aa336b5
nonon <nononcrust@gmail.com>
2022-10-20 23:40:35 +0900

    feat: LoadingSpinner 컴포넌트 추가

---
+++ src/components/common/LoadingSpinner.tsx
@@ -0,0 +1,56 @@
+import { theme } from '@lib/styles/theme';
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  .loader,
+  .loader:after {
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+  }
+  .loader {
+    position: relative;
+    border-top: 0.5rem solid ${theme.primary};
+    border-right: 0.5rem solid rgba(255, 255, 255, 0.2);
+    border-bottom: 0.5rem solid rgba(255, 255, 255, 0.2);
+    border-left: 0.5rem solid #ffffff;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation: load8 0.8s infinite linear;
+    animation: load8 0.8s infinite linear;
+  }
+
+  @-webkit-keyframes load8 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes load8 {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const LoadingSpinner: React.FC = () => {
+  return (
+    <Container>
+      <div className="loader" />
+    </Container>
+  );
+};
+
+export default LoadingSpinner;
