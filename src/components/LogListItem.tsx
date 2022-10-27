a1084c82b15d9cc3d186f58a0932d444c190a63b
nonon <nononcrust@gmail.com>
2022-10-27 18:00:53 +0900

    chore: LogListItem 스타일 추가

--- src/components/LogListItem.tsx
+++ src/components/LogListItem.tsx
@@ -1,18 +1,34 @@
-import { blue } from '@lib/styles/palette';
+import { gray } from '@lib/styles/palette';
+import { theme } from '@lib/styles/theme';
+import { format } from 'date-fns';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Flexbox from '@components/common/Flexbox';
+import FavoriteToggleButton from '@components/FavoriteToggleButton';
+
+import Card from './common/Card';
 
 const Container = styled.li`
-  display: flex;
-  justify-content: space-between;
-  padding: 1.5rem;
-  transition: 0.1s;
+  margin-top: 1rem;
+  border-radius: 0.25rem;
+  border-left: 6px solid ${theme.primary};
 
   &:hover {
-    background-color: ${blue[50]};
+    box-shadow: ${theme.elevation2};
+    transform: translateY(-2px);
+    transition: 0.1s;
+  }
+
+  .log-id {
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: ${gray[500]};
+  }
+
+  .log-date-location {
+    color: ${gray[400]};
   }
 `;
 
@@ -20,17 +36,31 @@ interface Props {
   logId: string;
   location: string;
   date: string;
+  isFavorite: boolean;
 }
 
-const LogListItem: React.FC<Props> = ({ logId, location, date }) => {
+const LogListItem: React.FC<Props> = ({
+  logId,
+  location,
+  date,
+  isFavorite,
+}) => {
   return (
     <Link to={`/logs/${logId}`}>
       <Container>
-        <Flexbox gap="1rem">
-          <p>#{logId}</p>
-          <p>{location}</p>
-        </Flexbox>
-        <p>{date}</p>
+        <Card>
+          <Flexbox flex="col" gap="1rem" items="start" padding="1rem">
+            <Flexbox gap="1rem">
+              <p className="log-id">
+                <span style={{ color: theme.primary }}>#</span> {logId}
+              </p>
+              <FavoriteToggleButton isFavorite={isFavorite} />
+            </Flexbox>
+            <p className="log-date-location">
+              {format(new Date(date), 'yyyy년 MM월 dd일')}, {location}
+            </p>
+          </Flexbox>
+        </Card>
       </Container>
     </Link>
   );
